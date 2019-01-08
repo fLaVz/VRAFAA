ad083ceb652ebae9e72a6a31a03b351e2c44a03e
flavz <juliendelvaux@neuf.fr>
2019-01-08 17:49:28 +0100

    :fire: removed unused files

--- components/AddScreen.js
+++ components/AddScreen.js
@@ -8,7 +8,6 @@ export default class AddScreen extends React.Component {
 
     constructor(props) {
         super(props)
-        
         this.state = {
             name: '',
             video: null,
